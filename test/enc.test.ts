626f755cf10b5b60aca71f63e8a333519f194c9d
echosoar <82163514@qq.com>
2023-09-09 12:32:21 +0800

    feat: enctype

--- test/enc.test.ts
+++ test/enc.test.ts
@@ -16,8 +16,10 @@ enc<
     const result = mdps.getResult();
     writeFileSync(resolve(__dirname, './json/enc_common.json'), JSON.stringify(result, null, '  '));
     expect(
-      result[0].type === 'task' &&
-      result[0].childs[0].type === 'item'
+      result[0].type === 'enc' &&
+      result[0].childs[0].type === 'line' &&
+      result[0].childs[0].childs[0].type === 'text' &&
+      result[0].childs[0].childs[0].value === '1111'
     ).toBeTruthy();
   });
 });
